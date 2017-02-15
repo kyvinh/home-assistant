9975bd66b915f0ed2f8f8651d70dacf0f06b7ace
kyddys <kyvinh@gmail.com>
2017-02-15 22:44:34 +0100

    bugfix: python syntax error

--- homeassistant/components/switch/command_line.py
+++ homeassistant/components/switch/command_line.py
@@ -146,7 +146,7 @@ class CommandSwitch(SwitchDevice):
 
     def turn_on(self, **kwargs):
         """Turn the device on."""
-        if (self._state)
+        if (self._state):
             return
         if (CommandSwitch._switch(self._command_on) and
                 not self._command_state):
@@ -155,7 +155,7 @@ class CommandSwitch(SwitchDevice):
 
     def turn_off(self, **kwargs):
         """Turn the device off."""
-        if (not self._state)
+        if (not self._state):
             return
         if (CommandSwitch._switch(self._command_off) and
                 not self._command_state):
